1ea1b5cc21a3ffb269995192b4910d40864dfac8
erickperejil <erickdanielescoto@gmail.com>
2025-07-25 11:45:07 -0600

    actualizacion de usuario

--- src/test/java/com/web/hn/users/User.java
+++ src/test/java/com/web/hn/users/User.java
@@ -1,11 +1,18 @@
 package com.web.hn.users;
 
 public class User {
-    public static String username = "user";
-    public static String password = "password";
+
+    public static String username = getUsername();
+    public static String password = getPassword();
 
     public static String[] cuentasAhorro = {"2000", "3000"};
     public static String[] tarjetasCredito = {"2000", "3000"};
     public static String[] cuentasCheques = {"2000", "3000"};
 
+    public static String getUsername() {
+        return System.getProperty("test.user", "defaultUser");
+    }
+    public static String getPassword() {
+        return System.getProperty("test.password", "defaultPassword");
+    }
 }
